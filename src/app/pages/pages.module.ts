5d25bb48bc184a1cf5101d0ab7ab6de06a4dbe82
@devjunior7titanq <devjunior7titanq@gmail.com>
2023-05-22 10:00:38 -0500

    avance listar y actualizar visita

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { UpdateListVisitsComponent } from './update-list-visits/update-list-visits.component';
 
 @NgModule({
   imports: [
@@ -19,6 +20,7 @@ import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
   ],
   declarations: [
     PagesComponent,
+    UpdateListVisitsComponent,
   ],
 })
 export class PagesModule {
---
+++ src/app/pages/update-list-visits/update-list-visits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-update-list-visits',
+  templateUrl: './update-list-visits.component.html',
+  styleUrls: ['./update-list-visits.component.scss']
+})
+export class UpdateListVisitsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
